c46bf748b6c5883b20bc2ac6ebf551aec17f5a9c
jitnesh verma <sonuvermavs143@gmail.com>
2026-08-03 23:58:15 +0530

    chang api

--- Backend/Confige/passport.js
+++ Backend/Confige/passport.js
@@ -6,7 +6,7 @@ import jwt from "jsonwebtoken";
 passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://localhost:3000/api/users/google/callback"
+        callbackURL: "https://airbnb-colness.onrender.com/api/users/google/callback"
     },
     async(accessToken, refreshToken, profile, done) => {
         try {
