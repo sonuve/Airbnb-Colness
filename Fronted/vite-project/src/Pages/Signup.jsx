2e6d77a3735b38607f0823ad69474914725a1faa
jitnesh verma <jitneshverma@jitneshs-MacBook-Air.local>
2026-08-07 00:30:24 +0530

    Add gogle signup

--- Fronted/vite-project/src/Pages/Signup.jsx
+++ Fronted/vite-project/src/Pages/Signup.jsx
@@ -2,6 +2,7 @@ import React, { useState, useCallback, memo } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import toast from "react-hot-toast";
+import { FcGoogle } from "react-icons/fc";
 import { IoMdEye, IoIosEyeOff } from "react-icons/io";
 const API_URL = import.meta.env.VITE_API_URL;
 function Signup() {
